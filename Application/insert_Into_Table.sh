8049952642dc4f0fb5725543b0cac9c8ae83361b
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-01-28 23:30:24 +0200

    addinsertScript

--- Application/insert_Into_Table.sh
+++ Application/insert_Into_Table.sh
@@ -1,1 +1,41 @@
-echo "insert into table"
+#!/usr/bin/bash
+
+
+
+echo "
+-----> Select your table number that you want to  insert into <--------
+"
+array=(`ls`)
+# echo ${#array[*]}
+
+select choice in  ${array[*]}
+do
+	if [ $REPLY -gt ${#array[*]} ]
+	then
+		echo "
+		$REPLY is not on the menu
+		"
+		continue
+	else
+		echo "
+		... You selected ${array[${REPLY}-1]} Table...
+		"
+			table_name=${array[${REPLY}-1]} 
+		break 
+	fi
+done	
+
+
+
+let num_fields=`head -1 $table_name | awk -F: '{print NF}'`
+
+for ((i=1;i<$num_fields;i++))
+do
+	
+	echo "insert filed num $i"
+	read field
+
+	row+=$field:
+done
+echo $row >> $table_name
+echo "the data inserted Sucssuflly"
