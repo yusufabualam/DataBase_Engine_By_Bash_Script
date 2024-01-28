aebe6ebbe64f2ba47c6bd9e93a65bf001b4c97d8
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-01-28 02:26:58 +0200

    createDatabase

--- Application/Create_DB.sh
+++ Application/Create_DB.sh
@@ -1,3 +1,40 @@
 #!/usr/bin/bash
 
-echo " -------- Hello from Creating Database -------- "
+cd  ./DataBase_Engine_By_Bash_Script
+
+
+while true 
+do
+echo "Write The Name Of The Database" 
+read db_Name
+
+case $db_Name in
+'')
+echo "the name canot be empty"
+continue;;
+*[[:space:]]  | *[[:space:]]* | [[:space:]]*) 
+echo  "the name can not have spaces"
+continue ;;
+
+[0-9]*)
+echo "the name can not contain integers"
+continue;;
+*[a-zA-Z_]*[a-zA-Z_] | [a-zA-z_] )
+if (find $db_Name 'ls -F | grep /' &> /dev/null 2>&1)
+then
+echo "already existing database"
+continue
+ else
+mkdir $db_Name
+break
+fi ;;
+*) 
+echo "write a valid name"
+continue;;
+
+esac
+done
+
+
+
+cd - &> /dev/null 2>&1 
