89c54e9b5b30ac5a1008b85f5517c3dfc301dc84
shahdahmedabdelraoufmohamed <shahd3a7med@gmail.com>
2024-02-02 22:03:58 +0200

    list insert editing

--- Application/listDB.sh
+++ Application/listDB.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 cd ../Data
-<<<<<<< HEAD
 
 
 if [ -z "$(ls -F | grep /)" ]; then
@@ -22,13 +21,11 @@ else
 
     ls -F  | grep / | tr / " " 
 fi
-
-=======
 echo -e "\n\x1b[1;33m --------  the Existing Database -------- \x1b[0m\n"
 counter=1
 ls -F | grep / | tr -d '/' | while read -r directory; do
     echo "$counter. $directory"
     ((counter++))
 done
->>>>>>> 54e37ad0ceb0526bcc42d659394b9d5f063d8ff4
+
 cd - &> /dev/null
